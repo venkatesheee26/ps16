a61f6aab5a89ffa143e25e86d2b52ead30592cc2
venkatesheee26@gmail.com <venkatesheee26@gmail.com>
2023-07-26 15:27:09 +0530

    ps16

---
+++ Class.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_16
+{
+    internal class BankAccount
+    {
+        private static int accountCounter = 0;
+
+        public string AccountNumber { get; }
+        public string AccountHolderName { get; set; }
+        private decimal Balance { get; set; }
+
+        public BankAccount(string accountHolderName)
+        {
+            AccountNumber = GenerateAccountNumber();
+            AccountHolderName = accountHolderName;
+            Balance = 9000;
+        }
+        public decimal GetBalance()
+        {
+            return Balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance.");
+            }
+        }
+
+        private static string GenerateAccountNumber()
+        {
+            accountCounter++;
+            return $"3689908723451234{accountCounter:3}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("Dhana Lakshmi");
+
+            Console.WriteLine("Account Details:");
+            Console.WriteLine($"Account Number: {account.AccountNumber}");
+            Console.WriteLine($"Account Holder Name: {account.AccountHolderName}");
+            Console.WriteLine($"Balance: {account.GetBalance()}");
+
+            Console.WriteLine();
+
+            account.Deposit(3000);
+            Console.WriteLine("After depositing $2000:");
+            Console.WriteLine($"Balance: {account.GetBalance()}");
+
+            Console.WriteLine();
+
+            account.Withdraw(500);
+            Console.WriteLine("After withdrawing $100:");
+            Console.WriteLine($"Balance: {account.GetBalance()}");
+
+            Console.WriteLine();
+
+            account.Withdraw(1000);
+            Console.WriteLine("After withdrawing $2000:");
+            Console.WriteLine($"Balance: {account.GetBalance()}");
+        }
+    }
+}
+
+
+
